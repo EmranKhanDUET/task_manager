0205d7acb55c69f37f5c1e69d1c0ee22a06f2465
emranDuetEee <emrankhanent@gmail.com>
2024-06-10 10:10:51 +0600

    initial commit

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/utility/app_colors.dart';
 import 'package:task_manager/ui/widgets/background_widget.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -9,34 +11,77 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passTEController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: BackgroundWidget(child: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                SizedBox(height: 80),
-                Text('Get Started With'),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Name'
+    return Scaffold(
+      body: BackgroundWidget(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 80),
+                  Text(
+                    'Get Started With',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  decoration: InputDecoration(hintText: 'Password'),
-                ),
-                SizedBox(height: 8),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined)),
-              ],
+                  const SizedBox(height: 24),
+                  TextField(
+                    controller: _emailTEController,
+                    decoration: const InputDecoration(hintText: 'E-Mail'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: _passTEController,
+                    decoration: const InputDecoration(hintText: 'Password'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                  const SizedBox(height: 36),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(onPressed: () {}, child: const Text('Forgot Password?')),
+                        RichText(
+                          text: TextSpan(
+                              text: "Don't have an account? ",
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(.8),
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: .4),
+                              children: [
+                                TextSpan(
+                                    text: 'Sign up',
+                                    style: const TextStyle(
+                                      color: AppColors.themeColor,
+                                    ),
+                                    recognizer: TapGestureRecognizer()..onTap = () {})
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      )),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passTEController.dispose();
+    super.dispose();
   }
 }
--- lib/ui/utility/app_colors.dart
+++ lib/ui/utility/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  static const Color appColor= Colors.green;
+  static const Color themeColor= Colors.green;
   static const Color white= Colors.white;
 }
